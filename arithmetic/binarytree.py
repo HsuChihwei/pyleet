f63dc068bd30046ca2b12c6498c954c12ba6a9a0
HsuChihwei <qwxuzhiwei@163.com>
2016-11-17 17:45:46 +0800

    增加4种遍历方式

--- arithmetic/binarytree.py
+++ arithmetic/binarytree.py
@@ -11,6 +11,7 @@ class Tree(object):
 	"""树类"""
 	def __init__(self):
 		self.root = Node()
+		self.dataList = []
 	def add(self,elem):
 		"""为树增加节点"""
 		node = Node(elem)
@@ -35,12 +36,66 @@ class Tree(object):
 					#如果本节点左右子节点均存在,加入队列,开始下次遍历
 					treeQueue.append(temp.lchild)
 					treeQueue.append(temp.rchild)
+	
+	def front(self):
+		"""深度优先遍历之先序遍历"""
+		self.front_f(self.root)
+	
+	def front_f(self,root):
+		"""先序遍历之嵌套"""
+		if root==None:
+			return
+		self.dataList.append(root.elem)
+		self.front_f(root.lchild)
+		self.front_f(root.rchild)
+
+	def middle(self):
+		"""深度优先遍历之中序遍历"""
+		self.middle_m(self.root)
+	
+	def middle_m(self,root):
+		"""中序遍历之嵌套"""
+		if root == None:
+			return
+		self.middle_m(root.lchild)
+		self.dataList.append(root.elem)
+		self.middle_m(root.rchild)
+
+	def last(self):
+		"""深度优先遍历之后序遍历"""
+		self.last_l(self.root)
+
+	def last_l(self, root):
+		"""后序遍历之嵌套"""
+		if root == None:
+			return 
+		self.last_l(root.lchild)
+		self.last_l(root.rchild)
+		self.dataList.append(root.elem)
+
+	def levelQueue(self):
+		"""广度优先遍历之层次遍历"""
+		if self.root == None:
+			return
+		treeQueue = []
+		treeQueue.append(self.root)
+		while(treeQueue):
+			temp = treeQueue.pop(0)
+			self.dataList.append(temp.elem)
+			if temp.lchild != None:
+				treeQueue.append(temp.lchild)
+			if temp.rchild != None:
+				treeQueue.append(temp.rchild)
 
 def main():
 	tree = Tree()
 	for i in range(10):
-		print(i)
 		tree.add(i)
+	# tree.front()
+	# tree.middle()
+	# tree.last()
+	tree.levelQueue()
+	print tree.dataList
 
 if __name__ == "__main__":
 	main()
